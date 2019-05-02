0e39c92878c27463cdced7039676db295bf15fb2
natemilner <milner.nathan@gmail.com>
2019-05-02 15:19:00 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+		<title>Guidelines | Berkshire Hathaway GUARD Insurance Companies</title>
+		
+	</head>
+	<body>
+	<div style="width: 700px; margin: 0 auto;">
+		<p>Welcome! Our goal is to provide an open and engaging forum to share, discuss and learn about GUARD and our products and services. Please be respectful of the opinions expressed by participants and keep these Guidelines in mind when posting:</p>
+		<p>If you have a specific question regarding your policy or claim, please contact us directly so that we may assist you. </p>
+		<p>Be safe: Protect the privacy of your personal information and the information of others; do not share or post contact, financial, policy or claim information. </p>
+		<p>Stay on topic: Comments should be relevant to our posts and discussions taking place. </p>
+		<p>Be considerate: Use of obscene, derogatory, hateful, discriminatory or offensive language, comments, or images are unacceptable. </p>
+		<p>Do not solicit: Third-party advertisements and posts that promote or solicit are not allowed. </p>
+		<p>We reserve the right to remove posts that do not follow the above Guidelines or that we deem in any way to be disruptive, misleading, inappropriate or infringe any intellectual property or any rights of others. If an individual’s disruptive behavior persists, we may permanently block that person from participating. While we are excited to hear from everyone, it is important to note that postings by fans to GUARD Facebook page do not necessarily reflect the opinions of GUARD, nor does GUARD in any way warrant their accuracy. </p>
+		<p>As a company policy, GUARD and its employees do not accept or consider unsolicited ideas, including but not limited to ideas for new, innovative or improved products or services, processes, advertising campaigns, marketing plans, or new product names. Accordingly, any information provided to GUARD shall be deemed non-confidential and GUARD shall be free to use such information on an unrestricted basis, without any compensation to you.</p>
+		<p>Please remember: While GUARD maintains a presence on Facebook, our official GUARD page, facebook.com/GUARD, is hosted by Facebook and subject to their terms of use. Facebook is a third-party entity unaffiliated with GUARD. GUARD is not responsible for the use of or for the privacy or security policies or content of Facebook or any other third-party site that may be linked to from Facebook. Please consider reviewing the privacy and legal notices of all websites prior to use. By posting to this page, you agree to be bound by these Guidelines, as well as Facebook's terms of use and privacy guidelines.</p>
+		
+		
+	</div>
+		
+	</body>
+</html>
